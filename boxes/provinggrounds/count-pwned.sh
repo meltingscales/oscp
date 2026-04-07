fca0cb7047a68b30ac5fefac16f433a79b94b8e0
Henry Post <HenryFBP@gmail.com>
2026-04-07 14:12:48 -0500

    improve count-pwned.sh

--- boxes/provinggrounds/count-pwned.sh
+++ boxes/provinggrounds/count-pwned.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
 count=$(find . -maxdepth 1 -name '*.pwned' -type d | wc -l)
-echo "$count/30 boxes pwned"
+percent=$((count * 100 / 30))
+echo "$count/30 boxes pwned ($percent%)"
+
+stuck=$(find . -maxdepth 1 -name '*.stuck' -type d | wc -l)
+echo "$stuck boxes stuck on"
+
+noncompleted=$(find . -maxdepth 1 -mindepth 1 -type d ! -name '.*' ! -name '*.*' | wc -l)
+echo "$noncompleted boxes not complete"
